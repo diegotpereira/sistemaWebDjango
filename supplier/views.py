7b428d58de35fe5ba7d4fff06907d4cf1cd277be
Diego <diegoteixeirapereira@hotmail.com>
2020-12-23 15:36:55 -0300

    alteração no arquivo views.py adicionado metodo request

--- supplier/views.py
+++ supplier/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from .models import Supplier
 
 # Create your views here.
+def supplier_list(request):
+    suppliers = Supplier.objects.all()
+    context = {
+       'supplier_list': suppliers
+    }
+    return render(request, 'supplier/list.html', context)
