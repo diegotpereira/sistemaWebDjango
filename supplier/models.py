a7a69991ff6b8ea95413efe89a31f1604bde1377
Diego <diegoteixeirapereira@hotmail.com>
2020-12-23 16:02:47 -0300

    alteração no arquivo models.py adicionado url fornecedores

--- supplier/models.py
+++ supplier/models.py
@@ -2,6 +2,8 @@ from django.db import models
 
 # Create your models here.
 class Supplier(models.Model):
+
+    objects = models.Manager() 
     name = models.CharField(max_length=50, blank=False)
     phone = models.CharField(max_length=20, blank=True)
     email = models.CharField(max_length=100, blank=True)
